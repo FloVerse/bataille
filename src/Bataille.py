f1a901610b5b96efefdce7aacff58f9a57dde1b2
f2bendo <92117591+f2bendo@users.noreply.github.com>
2023-12-06 18:11:19 +0100

    Class Player et cas égalité

--- src/Bataille.py
+++ src/Bataille.py
@@ -1,12 +1,14 @@
 # Bataille.py
 import random
 
+from Player import Player
+
 
 class Bataille:
     def __init__(self):
         self.deck = []
-        self.player1 = []
-        self.player2 = []
+        self.player1 = Player()
+        self.player2 = Player()
         self.rounds = 0
 
     def create_deck(self):
@@ -22,30 +24,71 @@ class Bataille:
         """
         Distribue les cartes du jeu aux deux joueurs.
         """
-        self.player1 = random.sample(self.deck, k=len(self.deck) // 2)
-        self.player2 = [card for card in self.deck if card not in self.player1]
+        random.shuffle(self.deck)
+        starting_player = random.choice([self.player1, self.player2])
+        for card in self.deck:
+            starting_player.add_to_main_deck([card])
+            starting_player = self.player1 if starting_player == self.player2 else self.player2
 
-    def play_round(self):
+    def bataille(self):
         """
-        Joue une manche de bataille.
+        Joue une bataille.
         """
-        card1 = self.player1.pop(0)
-        card2 = self.player2.pop(0)
+        # On vérifie que les joueurs ont assez de cartes pour jouer une bataille
+        if len(self.player1.main_deck) < 2:
+            self.player2.add_to_reserve_deck(self.player1.main_deck)
+            self.player1.main_deck.clear()
+            return
+        elif len(self.player2.main_deck) < 2:
+            self.player1.add_to_reserve_deck(self.player2.main_deck)
+            self.player2.main_deck.clear()
+            return
+
+        # On prend une carte caché
+        card_hidden1 = self.player1.main_deck.pop(0)
+        card_hidden2 = self.player2.main_deck.pop(0)
+
+        # On prend la deuxième carte de chaque joueur
+        card1 = self.player1.main_deck.pop(0)
+        card2 = self.player2.main_deck.pop(0)
 
         print("Joueur 1:", card1)
         print("Joueur 2:", card2)
 
+        # Si joueur 1 gagne
         if card1[0] > card2[0]:
-            self.player1.append(card1)
-            self.player1.append(card2)
-            print("Joueur 1 remporte la manche.")
+            self.player1.add_to_reserve_deck([card_hidden1, card_hidden2, card1, card2])
+            print("Joueur 1 remporte la bataille.")
+        # Si joueur 2 gagne
         elif card1[0] < card2[0]:
-            self.player2.append(card1)
-            self.player2.append(card2)
-            print("Joueur 2 remporte la manche.")
+            self.player2.add_to_reserve_deck([card_hidden1, card_hidden2, card1, card2])
+            print("Joueur 2 remporte la bataille.")
+        # Egalité
         else:
-            self.player1.append(card1)
-            self.player2.append(card2)
+            self.bataille()
+            print("Égalité !")
+    def play_round(self):
+        """
+        Joue une manche de bataille.
+        """
+        # On prend la première carte de chaque joueur
+        card1 = self.player1.main_deck.pop(0)
+        card2 = self.player2.main_deck.pop(0)
+
+        print("Joueur 1:", card1)
+        print("Joueur 2:", card2)
+
+        # Si joueur 1 gagne
+        if card1[0] > card2[0]:
+            self.player1.add_to_reserve_deck([card1, card2])
+            print("Joueur 1 remporte la manche.")
+        # Si joueur 2 gagne
+        elif card1[0] < card2[0]:
+            self.player2.add_to_reserve_deck([card1, card2])
+            print("Joueur 2 remporte la manche.")
+        # Egalité
+        else:  # à traiter
+            self.bataille()
             print("Égalité !")
 
         self.rounds += 1
@@ -54,23 +97,41 @@ class Bataille:
         """
         Joue une partie complète de bataille.
         """
+        # On crée le jeu et on distribue les cartes
         self.create_deck()
         self.deal_cards()
-        print("Début de la partie de bataille !")
+
+        print("Début de la bataille !")
         print("==============================")
-        while len(self.player1) > 0 and len(self.player2) > 0:
-            random.shuffle(self.player1)
-            random.shuffle(self.player2)
+        while (len(self.player1.main_deck) + len(self.player1.reserve_deck)) > 0 and (len(self.player2.main_deck) + len(self.player2.reserve_deck)) > 0:
             print("\nManche", self.rounds + 1)
             print("--------------------")
-            print("Joueur 1:", len(self.player1), "cartes")
-            print("Joueur 2:", len(self.player2), "cartes")
+            print("Joueur 1:", len(self.player1.main_deck), "cartes en jeu", "||",
+                  "Joueur 2:", len(self.player2.main_deck), "cartes en jeu")
+
+            print("Joueur 1:", len(self.player1.reserve_deck), "cartes en réserves", "||",
+                  "Joueur 2:", len(self.player2.reserve_deck), "cartes en réserves")
+
+
+            # On joue une manche
             self.play_round()
+            #Si un joueur n'a plus de cartes mais qu'il a encore des cartes dans sa résérve
+            if len(self.player1.main_deck) == 0 and len(self.player1.reserve_deck) > 0:
+                random.shuffle(self.player1.reserve_deck)
+                self.player1.add_to_main_deck(self.player1.reserve_deck)
+                self.player1.reserve_deck.clear()
+                random.shuffle(self.player1.main_deck)
+            if len(self.player2.main_deck) == 0 and len(self.player2.reserve_deck) > 0:
+                random.shuffle(self.player2.reserve_deck)
+                self.player2.add_to_main_deck(self.player2.reserve_deck)
+                self.player2.reserve_deck.clear()
+                random.shuffle(self.player2.main_deck)
+
 
         print("==============================")
-        if len(self.player1) > len(self.player2):
+        if len(self.player1.main_deck) > len(self.player2.main_deck):
             print("Joueur 1 remporte la partie !")
-        elif len(self.player1) < len(self.player2):
+        elif len(self.player1.main_deck) < len(self.player2.main_deck):
             print("Joueur 2 remporte la partie !")
         else:
             print("La partie se termine par une égalité !")
